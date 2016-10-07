c56801b7624e8c73dc35382c41f4b62978450910
Ian Allison <iana@pims.math.ca>
2016-10-07 13:05:28 -0700

    Moved the pending behavior to accommodate long spawn
    Signed-off-by: Ian Allison <iana@pims.math.ca>

--- jupyterhub/user.py
+++ jupyterhub/user.py
@@ -99,6 +99,7 @@ class User(HasTraits):
     spawner = None
     spawn_pending = False
     stop_pending = False
+    waiting_for_response = False
     
     @property
     def authenticator(self):
@@ -251,7 +252,7 @@ class User(HasTraits):
         self.state = spawner.get_state()
         self.last_activity = datetime.utcnow()
         db.commit()
-        self.spawn_pending = False
+        self.waiting_for_response = True
         try:
             yield self.server.wait_up(http=True, timeout=spawner.http_timeout)
         except Exception as e:
@@ -278,6 +279,9 @@ class User(HasTraits):
                 ), exc_info=True)
             # raise original TimeoutError
             raise e
+        finally:
+            self.waiting_for_response = False
+            self.spawn_pending = False
         return self
 
     @gen.coroutine
